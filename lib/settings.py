d8d29b1b7f1057b92cddf48775b65ba9944d0926
Rain Xiao <rainxiao@fortinet.com>
2025-10-29 14:35:07 -0700

    make buffer size and search windows to be around 256KB

--- lib/settings.py
+++ lib/settings.py
@@ -8,7 +8,7 @@ SEND_COMMAND_TIMEOUT = 10
 
 # Infinite-output protection defaults
 # Maximum allowed buffer growth (characters) before we short-circuit searches
-MAX_BUFFER_GROWTH = 50_000
+MAX_BUFFER_GROWTH = 300_000
 # Characters between repetitive checks in DevConn.search()
 REPETITIVE_CHECK_INTERVAL = 5_000
 # Minimum consecutive repeats to consider output as infinite/repetitive
@@ -18,4 +18,4 @@ REPETITIVE_PATTERN_THRESHOLD = 50
 # Warn if the searched substring length (from pos) exceeds this number
 OUTPUT_SEARCH_WARN_THRESHOLD = 100_000
 # When warning, clamp the search window to the last N characters of the buffer (not before pos)
-OUTPUT_SEARCH_WINDOW_SIZE = 50_000
+OUTPUT_SEARCH_WINDOW_SIZE = 300_000
